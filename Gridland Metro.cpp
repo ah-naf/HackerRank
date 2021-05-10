fc1932526a6adc51b2995aca1d859a0f31d64b9c
Ahnaf Hasan Shifat <81911439+ah-naf@users.noreply.github.com>
2021-05-10 22:35:15 +0600

    Create Gridland Metro.cpp

---
+++ Gridland Metro.cpp
@@ -0,0 +1,81 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+|                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+#define print(a)                for(auto it: a) cout<<it<<" "
+#define all(a)                  a.begin(),a.end()
+#define sf(v)                   for(auto &it:v) cin>>it
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+int main()
+{
+    AHNAF;
+    ll n,m,k;cin>>n>>m>>k; ll ans = n*m;
+    map<ll, vp> v;
+    for(int i=0; i<k; i++) {
+        ll r,c1,c2;cin>>r>>c1>>c2;
+        if(v.find(r) == v.end()) {
+            vp tmp; tmp.pb(mp(c1,c2));
+            v[r] = tmp;
+        }
+        else {
+            vp tmp = v[r]; tmp.pb(mp(c1,c2));
+            v[r] = tmp;
+        }
+    }
+    for(auto it: v) {
+        vp tmp = it.ss; SORT(tmp);
+        //ll first = LLONG_MAX, last = 0;
+        for(int i=0; i<tmp.size(); i++) {
+            ll first = tmp[i].ff, last = tmp[i].ss;
+            while(i+1 < tmp.size()) {
+                if(tmp[i+1].ff <= last) last = max(last, tmp[i+1].ss);
+                else break;
+                i++;
+            }
+            ans -= (last-first+1);
+            //cout<<first<<" "<<last<<endl;
+        }
+    }
+    cout<<ans<<endl;
+
+}
